a220f8babfe81e38011ed2c845223d855befc67d
jmanndev <jmanndev@gmail.com>
2016-11-21 16:15:34 +1100

    Files added

---
+++ 11393269Calculator/Calculator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculatorProgram
+{
+    class Calculator
+    {
+        private List<string> calcList = new List<string>();
+
+        public Calculator()
+        {}
+
+        public void Calculate(string calculationInput)
+        {
+            LoadList(calculationInput);
+            CalculateDivideMultiplyModulus();
+            CalculateAddSubtract();
+            PrintList();
+        }
+
+        private void CalculateDivideMultiplyModulus()
+        {
+            int previousInt, nextInt, result = 0;
+            string currentStr;
+
+            for (int i = 0; i < calcList.Count; i++)
+            {
+                currentStr = calcList.ElementAt(i);
+
+                if (isDivideMultiplyModulus(currentStr))
+                {
+                    previousInt = Int32.Parse(calcList.ElementAt(i - 1));
+                    nextInt = Int32.Parse(calcList.ElementAt(i + 1));
+
+                    if (isMultiply(currentStr))
+                        result = checked(previousInt * nextInt);
+                    else if (isDivide(currentStr))
+                        result = checked(previousInt / nextInt);
+                    else if (isModulus(currentStr))
+                        result = checked(previousInt % nextInt);
+
+                    AddResultToList(i, result);
+                    i--;
+                }
+            }
+        }
+
+        private void CalculateAddSubtract()
+        {
+            int previousInt, nextInt, result = 0;
+            string currentStr;
+
+            for (int i = 0; i < calcList.Count; i++)
+            {
+                currentStr = calcList.ElementAt(i);
+
+                if (isAddSubtract(currentStr))
+                {
+                    previousInt = Int32.Parse(calcList.ElementAt(i - 1));
+                    nextInt = Int32.Parse(calcList.ElementAt(i + 1));
+
+                    if (isAdd(currentStr))
+                        result = checked(previousInt + nextInt);
+                    else if (isSubtract(currentStr))
+                        result = checked(previousInt - nextInt);
+
+                    AddResultToList(i, result);
+                    i--;
+                }
+            }
+        }
+
+        private void LoadList(string calculationInput)
+        {
+            string tempString = "";
+
+            for (int i = 0; i < calculationInput.Length; i++)
+            {
+                string character = calculationInput.ElementAt(i) + "";
+
+                if (tempString == "" || !isOperator(character))
+                {
+                    tempString += character;
+                }
+                else
+                {
+                    calcList.Add(tempString);
+                    tempString = "";
+                    calcList.Add("" + character);
+                }
+            }
+            // Final List.Add to add last string to list
+            calcList.Add(tempString);
+        }
+
+        private void AddResultToList(int index, int result)
+        {
+            calcList.RemoveAt(index);
+            calcList.Insert(index, result.ToString());
+            calcList.RemoveAt(index + 1);
+            calcList.RemoveAt(index - 1);
+        }
+
+        private void PrintList()
+        {
+            foreach (string str in calcList)
+                Console.Write(str);
+        }
+
+        private bool isOperator(string inputString)
+        {
+            return isDivideMultiplyModulus(inputString) || isAddSubtract(inputString);
+        }
+
+        private bool isDivideMultiplyModulus(string inputString)
+        {
+            return isDivide(inputString) || isMultiply(inputString) || isModulus(inputString);
+        }
+
+        private bool isAddSubtract(string inputString)
+        {
+            return isAdd(inputString) || isSubtract(inputString);
+        }
+
+        private bool isDivide(string inputString)
+        {
+            return inputString == "/";
+        }
+
+        private bool isMultiply(string inputString)
+        {
+            return inputString == "*";
+        }
+
+        private bool isModulus(string inputString)
+        {
+            return inputString == "%";
+        }
+
+        private bool isAdd(string inputString)
+        {
+            return inputString == "+";
+        }
+
+        private bool isSubtract(string inputString)
+        {
+            return inputString == "-";
+        }
+
+    }
+}
---
+++ 11393269Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static int CALCULATIONINPUT = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator by J Mann 11393269\n");
+
+            if (args.Length == 1)
+            {
+                RunCalculator(args[CALCULATIONINPUT]);
+            }
+            else DisplayError("Must have exactly 1 argument for calculation (don't use spaces)\nExample: 2+3*4-1");
+        }
+
+        static private void RunCalculator(string inputString)
+        {
+            try
+            {
+                Calculator calc = new Calculator();
+                calc.Calculate(inputString);
+            }
+            catch (FormatException)
+            {
+                DisplayError("Invalid input format");
+            }
+            catch (DivideByZeroException)
+            {
+                DisplayError("Attempting to divide by zero");
+            }
+            catch (OverflowException)
+            {
+                DisplayError("Calculation is out of range");
+            }
+            catch (Exception)
+            {
+                DisplayError("Unexpected error");
+            }
+               
+        }
+
+        static private void DisplayError(string errorMessage)
+        {
+            Console.WriteLine("Calculator failed with the following error:");
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
